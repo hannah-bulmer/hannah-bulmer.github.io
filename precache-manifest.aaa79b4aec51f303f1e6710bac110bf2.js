7223c862b0c9e513658e1041fa1cf598f911821f
Hannah <hannah@MacBook-Pro-5.local>
2019-07-18 15:47:29 -0400

    Updates

---
+++ precache-manifest.aaa79b4aec51f303f1e6710bac110bf2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a033e7a0e3ce9145477",
+    "url": "/static/css/main.4da3e4d6.chunk.css"
+  },
+  {
+    "revision": "9a033e7a0e3ce9145477",
+    "url": "/static/js/main.9a033e7a.chunk.js"
+  },
+  {
+    "revision": "2d9884c03a6e947cc815",
+    "url": "/static/css/1.5d4362d9.chunk.css"
+  },
+  {
+    "revision": "2d9884c03a6e947cc815",
+    "url": "/static/js/1.2d9884c0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b92bd24a7015de99d8f0507dec863c84",
+    "url": "/static/media/github.b92bd24a.svg"
+  },
+  {
+    "revision": "f550e47e4e9dbf23bcefd02a034ca48a",
+    "url": "/static/media/campfire.f550e47e.gif"
+  },
+  {
+    "revision": "383fc36aad90e79afe068f4db788d49d",
+    "url": "/static/media/earth.383fc36a.gif"
+  },
+  {
+    "revision": "d9dea141f9ef7b9f4be257ad20247f84",
+    "url": "/static/media/drive.d9dea141.gif"
+  },
+  {
+    "revision": "ae0b6a80bba7c27aa5e21cbe506ad4f4",
+    "url": "/static/media/wave.ae0b6a80.gif"
+  },
+  {
+    "revision": "4c2aa379669385f999da124a75cc29e4",
+    "url": "/index.html"
+  }
+];
