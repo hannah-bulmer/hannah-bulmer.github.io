e295d849975e02e4dfde1b3cadb017c95b0a66f2
Hannah Bulmer <hannah.bulmer@elastic.io>
2019-12-14 17:50:13 +0100

    Website 2.0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import ParticleComponent from "./ParticleComponent";
 import '../node_modules/react-linechart/dist/styles.css';
 import './App.scss';
+import './birds.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import WebsiteLink from './components/WebsiteLink';
 
 const links = [
-  { link: 'https://www.linkedin.com/in/hannah-bulmer-099030154/', image: require('./static/linkedin.png')},
-  { link: 'https://github.com/hannah-bulmer', image: require('./static/github.svg')},
-  { link: 'https://drive.google.com/file/d/1Sk6aq4FoDJpWHWhIIdHPYcvIorGKTY0c/view', image: require('./static/resume.png')},
+  { link: 'https://www.linkedin.com/in/hannah-bulmer-099030154/', image: require('./static/linkedin.png') },
+  { link: 'https://github.com/hannah-bulmer', image: require('./static/github.svg') },
+  { link: 'https://drive.google.com/file/d/1Sk6aq4FoDJpWHWhIIdHPYcvIorGKTY0c/view', image: require('./static/resume.png') },
 ]
 
 class App extends Component {
@@ -18,15 +18,40 @@ class App extends Component {
     )
   }
 
+  renderBirds() {
+    return (
+      <div>
+        <div class="bird-container bird-container--one">
+          <div class="bird bird--one"></div>
+        </div>
+        <div class="bird-container bird-container--two">
+          <div class="bird bird--two"></div>
+        </div>
+
+        <div class="bird-container bird-container--three">
+          <div class="bird bird--three"></div>
+        </div>
+
+        <div class="bird-container bird-container--four">
+          <div class="bird bird--four"></div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
-        <div className="App">
-          <div className="particles-background">
-          <div className="title">Hannah Bulmer</div>
-          <h4>Aspiring computer scientist • Coffee enthusiast</h4>
-          {this.renderLinks()}
-          <ParticleComponent />
+        <div className="App" id="home">
+          <div className="title">Hannah Bulmer
+          <h4>Nature lover • Coffee enthusiast</h4>
+            {this.renderBirds()}
+          </div>
+          <div className="vertical">
+            {this.renderLinks()}
+          </div>
+          <div className="navigation">
+            <a href="#home">Home</a>
           </div>
         </div>
       </div>
