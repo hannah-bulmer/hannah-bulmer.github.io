708bbff898f74c60fccbd747177f0ee5a2c297ec
Hannah Bulmer <hannah.bulmer@elastic.io>
2019-12-16 22:29:55 +0100

    Add experience page, info card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import '../node_modules/react-linechart/dist/styles.css';
 import './App.scss';
-import './birds.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import WebsiteLink from './components/WebsiteLink';
+import ExperienceCard from './components/ExperienceCard';
 
 const links = [
   { link: 'https://www.linkedin.com/in/hannah-bulmer-099030154/', image: require('./static/linkedin.png') },
@@ -45,7 +44,7 @@ class App extends Component {
         <div className="App" id="home">
           <div>
             <div className="title">Hannah Bulmer</div>
-          <h4>Nature lover • Coffee enthusiast • Student at UW</h4>
+          <h4>Nature loving  🏕  Coffee enthusiast  ☕️  Developer 💻</h4>
             {this.renderBirds()}
           </div>
           <div className="vertical">
@@ -55,10 +54,23 @@ class App extends Component {
         <div className="navigation">
             <a href="#home">Home</a>
             <a href="#about">About</a>
+            <a href="#experience">Experience</a>
         </div>
 
         <div id="about">
-            Hello this is the about me section
+          <div className="container">
+            <div className="shadowBox"><img src={require('./static/hannah.jpg')} alt="Hannah" /></div>
+            <div className="rightCol">
+              <h3>Hi! I'm Hannah Bulmer.</h3>
+              <h5>
+                I'm a third year Computer Science student at the University of Waterloo.
+              </h5>
+            </div>
+          </div>
+        </div>
+
+        <div id="experience">
+          {/* <ExperienceCard /> */}
         </div>
       </div>
     );
---
+++ src/components/ExperienceCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ExperienceCard extends React.Component {
+  render() {
+    return(
+      <div className="container left">
+        Card
+      </div>
+    )
+  }
+}
+
+export default ExperienceCard;
