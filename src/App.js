6cb55424ac25831077d9f2ea18f453508992df21
Hannah Bulmer <hannah.bulmer@elastic.io>
2021-05-08 15:15:21 -0400

    Add background image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './App.scss'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Navigation, Internships, Home, About, Contact, Footer, Projects } from "./components"
 function App() {
