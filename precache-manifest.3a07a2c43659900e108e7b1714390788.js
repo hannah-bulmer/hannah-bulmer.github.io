d4cd213eb657b7999a8da817954daef60543d87b
Hannah Bulmer <hannah.bulmer@elastic.io>
2021-05-07 21:57:11 -0400

    Updates

--- precache-manifest.3a07a2c43659900e108e7b1714390788.js
+++ precache-manifest.3a07a2c43659900e108e7b1714390788.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "9a033e7a0e3ce9145477",
-    "url": "/static/css/main.4da3e4d6.chunk.css"
+    "revision": "d8f54e62649ef595f597",
+    "url": "/static/css/main.fe250851.chunk.css"
   },
   {
-    "revision": "9a033e7a0e3ce9145477",
-    "url": "/static/js/main.9a033e7a.chunk.js"
+    "revision": "d8f54e62649ef595f597",
+    "url": "/static/js/main.d8f54e62.chunk.js"
   },
   {
-    "revision": "2d9884c03a6e947cc815",
-    "url": "/static/css/1.5d4362d9.chunk.css"
+    "revision": "d90b8dc5a9e1c6a8822d",
+    "url": "/static/css/1.fbdd9e88.chunk.css"
   },
   {
-    "revision": "2d9884c03a6e947cc815",
-    "url": "/static/js/1.2d9884c0.chunk.js"
+    "revision": "d90b8dc5a9e1c6a8822d",
+    "url": "/static/js/1.d90b8dc5.chunk.js"
   },
   {
     "revision": "229c360febb4351a89df",
@@ -40,7 +40,7 @@ self.__precacheManifest = [
     "url": "/static/media/wave.ae0b6a80.gif"
   },
   {
-    "revision": "4c2aa379669385f999da124a75cc29e4",
+    "revision": "e551ed67b44f4fd881125d8d7313572a",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aaa79b4aec51f303f1e6710bac110bf2.js"
+  "/precache-manifest.3a07a2c43659900e108e7b1714390788.js"
 );
 
 workbox.clientsClaim();
